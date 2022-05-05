ccc50b1dfe11509f954ed95013cdcf285b9971d9
Nathan <nathansanches890@gmail.com>
2022-05-05 12:13:55 -0300

    quinta commit

--- main.js
+++ main.js
@@ -10,3 +10,11 @@ function onScroll(){
         navigation.classList.remove("scroll");
     }
 }
+
+function openMenu(){
+    document.body.classList.add('menu-expanded');
+}
+
+function closeMenu(){
+    document.body.classList.remove('menu-expanded');
+}
