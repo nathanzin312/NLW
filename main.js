e32f50613fe28237f60fe3924a57155fa4718809
Nathan <nathansanches890@gmail.com>
2022-05-08 21:37:53 -0300

    11 commit

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
+window.addEventListener('scroll', onScroll);
+
+onScroll();
+// essa funçao esta sendo executada pois se a pagina for carregada em alguma parte diferente nao no home
+// ela execute as funçoes de mostrar a nav e se preciso mostrar o botao
 
 function onScroll(){
+    showNav();
+    showButton();
+    console.log(scrollY);
+}
+
+function showNav(){
     if(scrollY > 1 ){
         // console.log(scrollY);  escreve o valor do scrollY 
         // adiciona a classe scroll no id navigation
@@ -8,6 +19,16 @@ function onScroll(){
     else{
         // se  o scroll nao for maior que 1 ele vai remover a classe scroll
         navigation.classList.remove("scroll");
+    }
+}
+
+
+function showButton(){
+    if(scrollY>=600){
+       ToTopButton.classList.add('show');
+    }
+    else{
+        ToTopButton.classList.remove('show');
     }
 }
 
@@ -24,3 +45,5 @@ function closeMenu(){
         distance:'30px',
         duration:700,
     }).reveal('#home,img, .card ,#services, #about,#contato, img, footer');
+
+
