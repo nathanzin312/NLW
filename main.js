f855b6c37bbc9ff6a101638f837a0d2577a2c258
Nathan <nathansanches890@gmail.com>
2022-05-10 21:32:00 -0300

    11 comit

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 window.addEventListener('scroll', onScroll);
 
 onScroll();
+
 // essa funçao esta sendo executada pois se a pagina for carregada em alguma parte diferente nao no home
 // ela execute as funçoes de mostrar a nav e se preciso mostrar o botao
 
 function onScroll(){
     showNav();
     showButton();
+    menu();
     console.log(scrollY);
 }
 
@@ -30,6 +32,16 @@ function showButton(){
     else{
         ToTopButton.classList.remove('show');
     }
+    colorChange();
+}
+
+function colorChange(){
+    if(scrollY>=4824){
+        ToTopButton.classList.add('color');
+    }
+    else{
+        ToTopButton.classList.remove('color');
+    }
 }
 
 function openMenu(){
@@ -46,4 +58,20 @@ function closeMenu(){
         duration:700,
     }).reveal('#home,img, .card ,#services, #about,#contato, img, footer');
 
-
+function menu(){
+   if(scrollY<=1200){
+       l1.classList.add('borderMenu');
+       l2.classList.remove('borderMenu');
+       l3.classList.remove('borderMenu');
+   }
+   else if(scrollY>1200 && scrollY <2700){
+    l2.classList.add('borderMenu');
+    l1.classList.remove('borderMenu');
+    l3.classList.remove('borderMenu');
+   }
+   else{
+       l3.classList.add('borderMenu');
+       l1.classList.remove('borderMenu');
+       l2.classList.remove('borderMenu');
+   }
+}
